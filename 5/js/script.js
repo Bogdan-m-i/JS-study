2ee0b1987b1114c716f7fe113bec8710033a8ad2
Bogdan <bogdan.m.i@yandex.ru>
2020-04-24 15:38:12 +0500

    lesson 6

---
+++ 5/js/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu-item'),
+    body = document.body,
+    title = document.querySelector('#title'),
+    adv = document.querySelector('.adv');
+    promptDiv = document.querySelector('#prompt');
+
+menu.insertBefore(menuItem[2], menuItem[1]);
+
+newMenuItem = menuItem[0].cloneNode();
+newMenuItem.textContent = 'Пятый пункт';
+menu.appendChild(newMenuItem);
+
+body.style.background = 'url(img/apple_true.jpg) center no-repeat';
+body.style.backgroundSize = 'cover';
+
+title.textContent = 'Мы продаем только подлинную технику Apple';
+
+adv.remove();
+
+let answer = prompt('Как вам техника Apple?','');
+promptDiv.textContent = answer;
