777b03c4a5d21393699b2fb733c509716533b9d2
Bogdan <bogdan.m.i@yandex.ru>
2020-04-25 16:05:07 +0500

    budget-app start

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,24 @@
+var btnStart = document.querySelector('#start'),
+
+    valBudget = document.querySelector('.budget-value'),
+    valDayBudget = document.querySelector('.daybudget-value'),
+    valLevel = document.querySelector('.level-value'),
+    valExp = document.querySelector('.expenses-value'),
+    valOptExp = document.querySelector('.optionalexpenses-value'),
+    valInc = document.querySelector('.income-value'),
+    valMothSav = document.querySelector('.monthsavings-value'),
+    valYearSav = document.querySelector('.yearsavings-value'),
+
+    inpExp = document.querySelectorAll('.expenses-item'),
+
+    btnExp = document.querySelector('.expenses-item-btn'),
+    btnOptExp = document.querySelector('.optionalexpenses-btn'),
+    btnCountBudget = document.querySelector('.count-budget-btn'),
+
+    inpInc = document.querySelectorAll('#income'),
+    inpSav = document.querySelector('#savings'),
+    inpSum = document.querySelector('#sum'),
+    inpPercent = document.querySelector('#percent'),
+    inpYear = document.querySelector('.year-value'),
+    inpMonth = document.querySelector('.moth-value'),
+    inpDay = document.querySelector('.day-value');
