4dbadd9fbc718ca704c286912b8bb910d943c185
Bogdan <bogdan.m.i@yandex.ru>
2020-05-07 13:22:57 +0500

    lesson 10

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -30,4 +30,49 @@ window.addEventListener('DOMContentLoaded', function(){
                 }
             }
         });
+
+
+        //timer
+
+        let deadLine = '2020-05-07 12:00:00';
+
+        function getTimeRemaining(endTime){
+            let t = Date.parse(endTime) - Date.parse(new Date),
+                s = Math.floor((t/1000) % 60),
+                m = Math.floor((t/1000/60) % 60),
+                h = Math.floor((t/1000/60/60));
+
+                if (h < 10) h = '0' + h;
+                if (m < 10) m = '0' + m;
+                if (s < 10) s = '0' + s;
+
+            return {
+                'total' : t,
+                's' : s,
+                'm' : m,
+                'h' : h,
+            }
+        }
+
+        function setClock(id, endTime) {
+            let timer = document.getElementById(id),
+                hours = timer.querySelector('.hours'),
+                minutes = timer.querySelector('.minutes'),
+                seconds = timer.querySelector('.seconds'),
+                timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = getTimeRemaining(endTime);
+
+                if(t.total <= 0){
+                    clearInterval(timeInterval);
+                    t.h = t.m = t.s = '00';
+                }
+
+                hours.textContent = t.h;
+                minutes.textContent = t.m;
+                seconds.textContent = t.s;
+            }
+        }
+        setClock('timer', deadLine);
 });
